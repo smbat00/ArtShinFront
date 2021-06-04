8389e7150983689dc8a86beb7634b33ce4f799dd
smbat.am <smbat.amiraghyan@beeoncode.com>
2021-06-04 18:51:04 +0400

    designpush

--- src/components/home/Slider.js
+++ src/components/home/Slider.js
@@ -1,7 +1,8 @@
 import React  from "react";
 import Slider from "react-slick"
-import "~slick-carousel/slick/slick.css";
-import "~slick-carousel/slick/slick-theme.css";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import slider1 from './img/slider/1.jpg'
 import slider2 from './img/slider/2.jpg'
@@ -9,25 +10,32 @@ import slider3 from './img/slider/3.jpg'
 
 const MySlider = () => {
     const settings = {
-        dots: true,
-        infinite: true,
+        // dots: true,
+        // infinite: true,
         speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
+        arrows: false,
+
+        autoplaySpeed: 2000,
+
+        // slidesToShow: 1,
+        // slidesToScroll: 1
     }
+const SliderImgStyles = {
+        width: '100%',
+}
 
     return (
         <div >
-            <h2> Single Item</h2>
+            {/*<h2> Single Item</h2>*/}
             <Slider {...settings}>
-                <div>
-                    <img src={slider1} />
+                <div >
+                    <img src={slider1} style={SliderImgStyles}/>
                 </div>
                 <div>
-                    <img src={slider2} />
+                    <img src={slider2} style={SliderImgStyles}/>
                 </div>
                 <div>
-                    <img src={slider3} />
+                    <img src={slider3} style={SliderImgStyles}/>
                 </div>
 
             </Slider>
